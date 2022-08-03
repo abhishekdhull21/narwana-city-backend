b22257a5eeb894453050f83d1588373effa40547
Abhishek Dhull <abhishek.dhull@affluencesolutions.com>
2022-08-03 23:35:13 +0530

    cors added

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("./server");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/cards");
 
 var app = express();
-
+app.use(cors());
+app.options("*", cors());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
